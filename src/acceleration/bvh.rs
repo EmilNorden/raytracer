b27e36ac9464bd9e1802e9c1c6ef954f5794fc06
Emil Nordén <emilnorden@yahoo.se>
2026-04-20 21:24:48 +0200

    removed redundant(?) root bounds check

--- src/acceleration/bvh.rs
+++ src/acceleration/bvh.rs
@@ -5,7 +5,6 @@ use crate::scene::{Intersectable, Intersection};
 
 pub struct BVH {
     nodes: Vec<BVHNode>,
-    bounds: AABB,
 }
 
 #[derive(Default)]
@@ -39,16 +38,11 @@ struct Bin {
 
 impl BVH {
     pub fn new(items: &mut [MeshInstance]) -> Self {
-        let bbox = AABB::compound(items.iter().map(|m| {
-            m.bounds().transform(m.transform())
-        }));
-
         let mut nodes = Vec::new();
         Self::build_node(items, 0, &mut nodes);
 
         Self {
             nodes,
-            bounds: bbox,
         }
     }
 
@@ -63,9 +57,6 @@ impl BVH {
         t_min: f32,
         t_max: f32,
     ) -> Option<(u32, Intersection)> {
-        if self.bounds.intersect_closest(ray, t_max).is_none() {
-            return None;
-        }
         Self::traverse_bvh(&self.nodes, items, ray, t_min, t_max)
     }
 
